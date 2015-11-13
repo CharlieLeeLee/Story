2d16e17030cbbefeb7e05f6e092ee0040e8c8c3e
Charlie Norvell <charlie.norvell@gmail.com>
2015-11-13 09:43:19 -0500

    Login Cookies
    Added cookies to login page. Currently, cookies are set to expire after
    1 day. We should probably eventually implement the whole "click here to
    stay signed in" thing

--- Final Project/cgi-bin/login.py
+++ Final Project/cgi-bin/login.py
@@ -3,89 +3,138 @@
 # CSC210 - log in - 11/08/2015
 # Charlie Norvell
 
-# To run, start AMPSS and visit URLs like the following to insert new
-# entries into the database, then check your database's contents using
-# lecture4-query-database.py
-#
-# http://localhost/cgi-bin/lecture4.py?my_name=Joe&my_age=32&my_image=../cat.jpg
-# http://localhost/cgi-bin/lecture4.py?my_name=Donna&my_age=37&my_image=../dog.jpg
 
 # useful for debugging
 import cgitb
 cgitb.enable()
 
-import cgi
-form = cgi.FieldStorage()
+def notLoggedInPage():
+        import cgi
+        form = cgi.FieldStorage()
+        aname = form['accountname'].value
+        pw = form['password'].value
 
-aname = form['accountname'].value
-pw = form['password'].value
+        import datetime
+        t = str(datetime.datetime.now() + datetime.timedelta(day=1))
+        cookie = Cookie.SimpleCookie()
+        import sqlite3
 
+        conn = sqlite3.connect('accounts.db')
+        c = conn.cursor()
+        for r in c.execute('select * from accounts where aname=?;', [aname]):
+                if r[3] == pw:
+                        cookie['username'] = aname
+                        cookie['username']['expires'] = t.strftime('%a, %d %b %Y %H:%M:%S')
+                        print "Content-type: text/html"
+                        print cookie
+                        print
+                else:
+                        print "Content-type: text/html"
+                        print
+                        print '<p>Incorrect password</p>'
+        conn.close()
+        
+        print '<html>'
+        print ' <head>'
+        print '		<title>'
+        print '			Logging In...'
+        print '		</title>'
+        print '		<style type="text/css">'
+        # in Python, use ''' triple quotes ''' to create a multi-line string
+        print '''
+                                h1 {
+                                        font-size: 100px;
+                                        font-family: arial;
+                                        color: #337AB7;
+                                }
 
-# insert new user data into the database
-import sqlite3
+                                img {
+                                        width: 300px;
+                                }
 
-conn = sqlite3.connect('accounts.db')
-c = conn.cursor()
+                                h2 {
+                                        color: #337AB7;
+                                        font-family: arial;
+                                }
 
+                                h3 {
+                                        font-size: 20px;
+                                        font-family: arial;
+                                        color: #337AB7;
+                        </style>
 
-# print the http header
-print "Content-Type: text/html"
-print # don't forget the extra newline
+                </head>
+            
+        '''
+        print '<body>'
+        print'<p>You are not yet logged in.</p>'
+        print'<form method="post" action="login.py">'
+        print'Username:'
+        print'<input uname="username" type=text size="3-"/>'
+        print'Password:'
+        print'<input pass="password" type=text size="50"/>'
+        print'<input type="submit"/>'
+        print'</form>'
+        print'</body></html>'
 
-print '<html>'
-print ' <head>'
-print '		<title>'
-print '			Logging In...'
-print '		</title>'
-print '		<style type="text/css">'
-# in Python, use ''' triple quotes ''' to create a multi-line string
-print '''
-			h1 {
-				font-size: 100px;
-				font-family: arial;
-				color: #337AB7;
-			}
+def loggedInPage(username):
+        print '<html>'
+        print ' <head>'
+        print '		<title>'
+        print '			Logged In!'
+        print '		</title>'
+        print '		<style type="text/css">'
+        # in Python, use ''' triple quotes ''' to create a multi-line string
+        print '''
+                                h1 {
+                                        font-size: 100px;
+                                        font-family: arial;
+                                        color: #337AB7;
+                                }
 
-			img {
-				width: 300px;
-			}
+                                img {
+                                        width: 300px;
+                                }
 
-			h2 {
-				color: #337AB7;
-				font-family: arial;
-			}
+                                h2 {
+                                        color: #337AB7;
+                                        font-family: arial;
+                                }
 
-			h3 {
-                                font-size: 20px;
-                                font-family: arial;
-                                color: #337AB7;
-		</style>
+                                h3 {
+                                        font-size: 20px;
+                                        font-family: arial;
+                                        color: #337AB7;
+                        </style>
 
-	</head>
-    
-'''
-print '<body>'
-print '<h1>Logging In...</h1>'
+                </head>
+            
+        '''
+        print '<body>'
+        print '<h1>Hello ' + username +'</h1>'
+        print '<a href="profile.py?aname=' + username + '">Your Profile</a>'
+        print '</body></html>'
+        
+import Cookie
+import os        
 
-# print out the data for all users in the database
-notfound = True
-badpass = False
-for r in c.execute('select * from accounts;'):
-        if (r[0] == aname):
-            notfound = False
-            if(r[3] == pw):
-                print '<meta http-equiv="refresh"'
-                print ' content="0; url=profile.py?aname='+aname+'">'
-            else:
-                badpass = True
-                print '<h2> Incorrect Password</h2>'
-                print '<h2><a href="../SignIn.html">Try Again</a></h2>'
-if (notfound):
-    print '<h2>User '+aname+' does not exist.</h2>'
-    print '<h2><a href="../CreatAccount.html">Create New Account</a></h2>'
-elif (badpass == False):
-    print '<h2>Redirecting...</h2>'
-conn.close()
+# if not logged in, show login screen.
+stored_cookie_string = os.environ.get('HTTP_COOKIE')
+if not stored_cookie_string:
+        print "Content-type: text/html"
+        print
+        notLoggedInPage()
+else:
+        cookie = Cookie.SimpleCookie(stored_cookie_string)
+        if 'username' in cookie:
+                aname = cookie['username'].value
+
+                print "Content-type: text/html"
+                print
+                loggedInPage(aname)
+
+        else:
+                notLoggedInPage()
 
 print '	</body>'
 print '</html>'
